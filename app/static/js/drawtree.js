799f445c272a69f7e3a761c625fa15138f5499f0
Victoria Lim <lim.t.victoria@gmail.com>
2021-01-29 17:57:24 -0800

    edit canvas/tree/node sizes, shadow for nodes, position node text

--- app/static/js/drawtree.js
+++ app/static/js/drawtree.js
@@ -5,16 +5,14 @@ var iconUN = "/static/images/icon_un.png";
 var iconEU = "/static/images/icon_eu.png";
 var iconJP = "/static/images/icon_jp.png";
 
-var treeData = {"name" : "Root", "info" : "1", "img": iconUS,
-    "children" : [
-        {"name" : "A", "info" : "2", "img": iconUN },
-        {"name" : "B", "info" : "3", "img": iconEU },
-        {"name" : "C", "info" : "4", "img": iconJP, "children": [
-
-                {"name" : "C1", "info" : "5", "img": iconUS },
-                {"name" : "C2", "info" : "6 and I am a very long info", "img": iconEU }
-            ]}
-    ]};
+var treeData = {"appNo":"123456789", "pubNo":"abcdefgh", "title":"Do the best you can until you know better. Then when you know better, do better.", "img":iconUS, "children" : [
+    {"appNo":"234567891", "pubNo":"bcdefghi", "title":"Your level of success will seldom exceed your level of personal development.", "img":iconUN },
+    {"appNo":"345678912", "pubNo":"cdefghij", "title":"The most courageous decision that you can make each day is to be in a good mood.", "img":iconEU },
+    {"appNo":"456789123", "pubNo":"defghjik", "title":"Anger has a honey tip, but a poison source.", "img":iconJP, "children": [
+        {"appNo" : "567891234", "pubNo" : "efghijkl", "title" : "Success hinges less on getting everything right than on how you handle getting things wrong.", "img": iconUS },
+        {"appNo" : "678912345", "pubNo" : "fghijklm", "title" : "We don’t see things as they are; we see them as we are.", "img": iconEU }
+    ]}
+]};
 
 // Define diagonal for edge lines
 // VL update from https://stackoverflow.com/a/65819327
@@ -26,21 +24,21 @@ var diagonal = function link(d) {
 };
 
 // Define dimensions for svg canvas by size of browser window
+// marginh defines space bt tree and canvas edge
 // https://stackoverflow.com/q/16265123
 var canvas = {
   //width: 600,
   //height: 400,
-  width: window.screen.width / 2,
-  height: window.screen.height / 1.5,
-  marginw: window.screen.width / 10,
-  marginh: window.screen.height / 7,
+  width: window.screen.width / 1.3,
+  height: window.screen.height / 1.1,
+  marginh: window.screen.height / 6,
 };
 
 // Define properties for node rectangles
 // rx and ry define corner curvature
-var rectNode = {
-  width : canvas.marginw/1.05,
-  height : canvas.marginh/1.5,
+var rectnode = {
+  width : window.screen.width / 6.5,
+  height : window.screen.height / 6.5,
   textMargin : 5,
   rx : 6,
   ry : 6,
@@ -49,8 +47,8 @@ var rectNode = {
 
 // Define properties for icon size
 var icon = {
-  w: rectNode.height/1.4,
-  h: rectNode.height/1.4,
+  w: rectnode.height/1.7,
+  h: rectnode.height/1.7,
 };
 
 // Create a svg canvas (order matters)
@@ -60,17 +58,10 @@ var vis = d3.select("#outdiv").append("svg:svg")
             .attr("viewBox", "0 0 " + canvas.width + " " + canvas.height)
             .attr("preserveAspectRatio", "xMidYMid meet")
             .append("svg:g")
-            .attr("transform", "translate(-10, 90)");
-
-// Add tooltip div
-var div = d3.select("body").append("div")
-.attr("class", "tooltip")
-.style("opacity", 1e-6);
+            .attr("transform", "translate(-120, 130)");
 
 //  // Add border around the svg canvas
 //  var borderPath = vis.append("rect")
-//  .attr("x", -10)
-//  .attr("y", -90)
 //  .attr("width", canvas.width)
 //  .attr("height", canvas.height)
 //  .attr("class", "canvas-border");
@@ -78,7 +69,44 @@ var div = d3.select("body").append("div")
 // Create a tree "canvas"
 var treeMap = d3.tree()
 //.size([500, 300]);
-.size([canvas.width , canvas.height-2*canvas.marginh ]);
+.size([ canvas.width+canvas.marginh , canvas.height-2*canvas.marginh ]);
+
+// Add tooltip div
+var tdiv = d3.select("body").append("div")
+.attr("class", "tooltip")
+.style("opacity", 1e-6);
+
+// Define filter for black drop shadow
+// https://stackoverflow.com/a/26964445
+var defs = vis.append("defs");
+
+var filter = defs.append("filter")
+    .attr("id", "drop-shadow")
+filter.append("feGaussianBlur")
+    .attr("in", "SourceAlpha")
+    .attr("stdDeviation", 2)
+    .attr("result", "blur");
+filter.append("feOffset")
+    .attr("in", "blur")
+    .attr("dx", 4)
+    .attr("dy", 4)
+    .attr("result", "offsetBlur");
+filter.append("feFlood")
+    .attr("in", "offsetBlur")
+    .attr("flood-color", "#3d3d3d")
+    .attr("flood-opacity", "0.5")
+    .attr("result", "offsetColor");
+filter.append("feComposite")
+    .attr("in", "offsetColor")
+    .attr("in2", "offsetBlur")
+    .attr("operator", "in")
+    .attr("result", "offsetBlur");
+
+var feMerge = filter.append("feMerge");
+feMerge.append("feMergeNode")
+    .attr("in", "offsetBlur")
+feMerge.append("feMergeNode")
+    .attr("in", "SourceGraphic");
 
 // Preparing the data for the tree layout, convert data into an array of nodes
 // assigns the data to a hierarchy using parent-child relationships
@@ -116,52 +144,63 @@ var node = vis.selectAll("g.node")
 .enter().append("svg:g")
 .attr("transform",
   function(d) {
-    return "translate(" + (d.x-(rectNode.width/2)) + "," + (d.y-(rectNode.height/2)) + ")";
+    return "translate(" + (d.x-(rectnode.width/2)) + "," + (d.y-(rectnode.height/2)) + ")";
   }
 )
 
 // Add a rectangle at every node
 node.append("rect")
-.on("mouseover", mouseover)
-.on("mousemove", function(event,d){mousemove(event,d);})
-.on("mouseout", mouseout)
-.attr("width", rectNode.width)
-.attr("height", rectNode.height)
-.attr('rx', rectNode.rx)
-.attr('ry', rectNode.ry)
-.attr("fill","red");
+    .on("mouseover", mouseover)
+    .on("mousemove", function(event,d){mousemove(event,d);})
+    .on("mouseout", mouseout)
+    .attr("class", "rectnode")
+    .attr("width", rectnode.width)
+    .attr("height", rectnode.height)
+    .attr('rx', rectnode.rx)
+    .attr('ry', rectnode.ry)
+    .style("filter", "url(#drop-shadow)");
 
-node.append("svg:text")
-.on("mouseover", mouseover)
-.on("mousemove", function(event,d){mousemove(event,d);})
-.on("mouseout", mouseout)
-.attr("dx", rectNode.width/3)
-.attr("dy", rectNode.height/3)
-.text(function(d) { return d.data.name; })
+// Add text in two parts since new lines are hard in svg
+var textnode = node.append("svg:text")
+    .on("mouseover", mouseover)
+    .on("mousemove", function(event,d){mousemove(event,d);})
+    .on("mouseout", mouseout)
+    .style("fill", "white")
+    .style("font-size", "28px")
+textnode.append("tspan")
+    .text(d => "App: " + d.data.appNo)
+    .attr("x", rectnode.width/8) // use 0 for left edge of rectangles
+    .attr("dy", rectnode.height/2.6)
+textnode.append("tspan")
+    .text(d => "Pub: " + d.data.pubNo)
+    .attr("x", rectnode.width/8) // use 0 for left edge of rectangles
+    .attr("dy", rectnode.height/3)
 
 node.append("svg:image")
-.attr("xlink:href", function(d) { return d.data.img; })
-.attr("width", icon.w)
-.attr("height", icon.h)
-.attr('x', -20)
-.attr('y', -40)
+    .attr("xlink:href", function(d) { return d.data.img; })
+    .attr("width", icon.w)
+    .attr("height", icon.h)
+    .attr('x', -20)
+    .attr('y', -50);
 
+// tooltip fade-in
 function mouseover() {
-    div.transition()
+    tdiv.transition()
     .duration(300)
     .style("opacity", 1);
 }
 
 function mousemove(event, d) {
-    console.log(d);
-    div
-    .html("<b>Title: </b>" + d.data.info)
-    .style("left", (event.pageX ) + "px")
-    .style("top", (event.pageY - 15) + "px");
+    tdiv
+    //.html("<b>Title: </b>" + d.data.title)
+    .html(d.data.title)
+    .style("left", (event.pageX + 8) + "px")
+    .style("top", (event.pageY + 12) + "px");
 }
 
+//tooltip fade-out
 function mouseout() {
-    div.transition()
+    tdiv.transition()
     .duration(300)
     .style("opacity", 1e-6);
 }
