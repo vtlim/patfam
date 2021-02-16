ed648353bfbe0cf95f25fd44e109fc6de88a290f
Victoria Lim <lim.t.victoria@gmail.com>
2021-02-15 16:11:48 -0800

    format pubNo for direct navigation of patentscope; fix regex parsing of results

--- app/get_wipo_identifiers.py
+++ app/get_wipo_identifiers.py
@@ -34,17 +34,28 @@ def navigate_target_page(driver):
     search_class = "ps-biblio-data--biblio-card"
     try:
         mydata = driver.find_element_by_class_name(search_class)
+        mydata = mydata.text
     except NoSuchElementException as e:
         return "UNDEFINED"
 
+    # === extract priority data ===
     # get text bt "Priority Data" and "Publication Language"
-    priority_data = re.search('Priority Data(.*)Publication Language',
-        mydata).group(1)
+    # use re.DOTALL to search over multiple lines
+    priority_data = re.search(r'Priority Data(.*?)Publication Language',
+        mydata, re.DOTALL).group(1)
 
     # split the priority data, of form: 09/418,640 15.10.1999 US
-    clean_priority_data = priority_data.split()[0]
+    clean_priority_data = priority_data.split()[0].rstrip()
 
-    return clean_priority_data
+    # remove any spaces, punctuation (keep numbers/letters)
+    clean_priority_data = re.sub(r'[^a-zA-Z0-9]', '', clean_priority_data)
+
+    # === extract application number ===
+    # if navigated to target page, the input provided must've been pubno
+    app_num = re.search(r'International Application No.(.*?)International Filing Date',
+        mydata, re.DOTALL).group(1)
+
+    return clean_priority_data, app_num.strip()
 
 
 def get_wipo_identifiers(input_num, doc_type):
@@ -54,32 +65,45 @@ def get_wipo_identifiers(input_num, doc_type):
     use_in_url = False
 
     # if pubNo, circumvent the search page and use pubNo in URL directly
-    # works whether the number is WO2007* or WO07*
     if (input_num[:2].lower() == "wo") and (doc_type == "publication"):
-        try:
-            float(input_num[2:])
+
+        pub_num_pieces = input_num[2:].split('/')
+
+        # TODO generalize this
+        # add leading two digits of year, separate 1980-1999 and 2000-2079
+        if len(pub_num_pieces[0]) == 2:
+            if int(pub_num_pieces[0]) >= 80:
+                pub_num_pieces[0] = str(19) + str(pub_num_pieces[0])
+            else:
+                pub_num_pieces[0] = str(20) + str(pub_num_pieces[0])
+
+        # add padding zeros in beginning if < 6 digits
+        if len(pub_num_pieces[1]) < 6:
+            num_zeros = 6 - len(pub_num_pieces[1])
+            pub_num_pieces[1] = num_zeros*str(0) + str(pub_num_pieces[1])
+
+        doc_id = "WO" + str(pub_num_pieces[0]) + str(pub_num_pieces[1])
+        if len(doc_id) == 12:
             use_in_url = True
-        except ValueError:
-            pass
 
     # create a chrome webdriver
     driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 
     if use_in_url:
-        target_url = target_page_prefix + input_num
-        print(f"  Going to {target_url}")
+        target_url = target_page_prefix + doc_id
+        print(f"\tNavigating directly to {target_url}")
         obtain_target_page(driver, target_url)
 
     else:
-        print(f"  Searching {input_num} on {front_page_url}")
+        print(f"\tSearching {input_num} on {front_page_url}")
         navigate_front_page(driver, front_page_url, input_num)
 
     # assuming we're on target page, scrape biblio info
     # return is "UNDEFINED" if not on the target page or biblio missing
-    clean_priority_data = navigate_target_page(driver)
+    clean_priority_data, app_num = navigate_target_page(driver)
 
     if clean_priority_data == "UNDEFINED":
         return clean_priority_data
 
-    return { "parent": [clean_priority_data], "child": [], "uncategorized": [] }
+    return { "parent": [clean_priority_data], "child": [], "uncategorized": [app_num] }
 
